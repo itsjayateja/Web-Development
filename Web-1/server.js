a9de88853c5dc044ed5a62b777a91196d90464e7
Jaya Teja Bathula <56495773+itsjayateja@users.noreply.github.com>
2022-11-26 10:19:44 +0530

    calculator

--- Web-1/server.js
+++ Web-1/server.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html")
+    res.sendFile(__dirname + "/public/index.html")
 });
 
 app.post("/", function (req, res) {
@@ -15,7 +15,13 @@ app.post("/", function (req, res) {
     var numberTwo = Number(req.body.num2)
     var total = numberOne + numberTwo
     res.send("Thanks for sending " + total)
+    console.log(total)
 });
+
+app.post("/contact", function(req, res){
+    res.send("<h1>Hello World</h1>")
+    
+})
 
 app.listen(3000, function () {
     console.log("Server started at 3000 port")
